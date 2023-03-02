fb2d141c43319b9fd48a74ff59056fbb4dba4208
Krystian Wolniak <57046666+Orzyj@users.noreply.github.com>
2023-03-02 12:15:00 +0100

    Add files via upload

---
+++ List1/zad3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true){
+                Console.WriteLine("liczba {0} trojkatna", (Math.Sqrt(8 * int.Parse(Console.ReadLine()) + 1) % 1 == 0 ? "jest" : "nie jest"));
+            }
+        }
+    }
+}
