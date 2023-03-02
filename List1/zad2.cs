1f1c48b68d886133c785ba1d7e1494c1518bd891
Krystian Wolniak <57046666+Orzyj@users.noreply.github.com>
2023-03-02 12:03:55 +0100

    Add files via upload

---
+++ List1/zad2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                String s = Console.ReadLine();
+                Console.WriteLine("Wyraz {0}, {1}", s, (getStatus(s) ? "jest" : "nie jest"));
+            }
+        }
+
+        public static bool getStatus(string myString)
+        {
+            string first = myString.Substring(0, myString.Length / 2);
+            char[] arr = myString.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+
+    }
+}
