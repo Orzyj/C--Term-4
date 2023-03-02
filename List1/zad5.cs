75357d0aede29365742f7d1a34c142511967e595
Krystian Wolniak <57046666+Orzyj@users.noreply.github.com>
2023-03-02 21:59:25 +0100

    Add files via upload

---
+++ List1/zad5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n, k;
+
+            try
+            {
+                Console.WriteLine("Wprowadź liczbe K: ");
+                k = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Wprowadź stopień pierwiastka n: ");
+                n = int.Parse(Console.ReadLine());
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Pierwiastek {0}-tego stopnia z {1}, wynosi: {2}", n, k, fun(n, k));
+        }
+        private static double fun(int n, int k)
+        {
+            return Math.Pow(k, 1.0 / n);
+        }
+    }
+}
